b0a27050d9eb32ee1857d01fe425c853100549b4
nayoung.kim <jenny6816@naver.com>
2026-01-30 16:23:24 +0900

    feat: 카테고리 대분류 구현

--- gallery/admin.py
+++ gallery/admin.py
@@ -11,5 +11,9 @@ class PhotoAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'category', 'is_bookmarked', 'is_trashed', 'created_at']
     list_filter = ['user', 'category', 'is_bookmarked', 'is_trashed']
     search_fields = ['memo']
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = Category.objects.filter(user=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Notification)
