42b51509833d016c921aa5117bd5838e9b5fe5c2
hhd517 <han9959e@naver.com>
2026-02-01 16:06:36 +0900

    [fix] 홈 접속 시 로그인 페이지로 리다이렉트 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', lambda request: redirect('/accounts/login/')), 
-    
+
     path('admin/', admin.site.urls),
 
     # auth
