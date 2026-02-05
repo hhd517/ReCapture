67bb85ed9032e6a6d1bda89649ffa25a07926d9f
hhd517 <han9959e@naver.com>
2026-02-06 03:38:08 +0900

    [chore] .env 기반 환경변수 설정 정리 및 시크릿 키 분리

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-m#wcj!^3&hz&v3gz-z%yvxgng2_@r!8cg=c#i3&pt@+rt59=mh')
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY가 설정되지 않았습니다. .env 파일을 확인하세요.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
